432a60fafcb7e505dee7048bd746720164fe87ad
dhiraj-patel <dhirajsuperhero@gmail.com>
2017-02-13 14:04:36 -0500

    Html form created, and clear canvas button is functioning. Just need to finish up javascript part

--- canvas.js
+++ canvas.js
@@ -1,6 +1,11 @@
-
-
 var c = document.getElementById("brownies");
 var ctx = c.getContext("2d");
 ctx.fillStyle = "#ff0000";
 ctx.fillRect( 50, 50, 100, 200 );
+
+
+function clearDaCanvas(){
+    var canvas=document.getElementById("brownies");
+    var context=canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
