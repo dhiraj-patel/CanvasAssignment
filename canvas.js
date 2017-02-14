15e734f9893e6296956bfd6d0192270fe9731d41
dhiraj-patel <dhirajsuperhero@gmail.com>
2017-02-13 21:11:01 -0500

    finished assignemt  random rectangles occur on click now.

--- canvas.js
+++ canvas.js
@@ -1,11 +1,18 @@
 var c = document.getElementById("brownies");
 var ctx = c.getContext("2d");
-ctx.fillStyle = "#ff0000";
-ctx.fillRect( 50, 50, 100, 200 );
 
+
+function randRectangles(e){
+    var randWidth = 22+Math.random()*100;
+    var randHeight = 22+Math.random()*100;
+    ctx.fillStyle = "#EEE8AA";
+    ctx.fillRect(e.offsetX-25, e.offsetY - 25, 50, 50);
+}
 
 function clearDaCanvas(){
     var canvas=document.getElementById("brownies");
     var context=canvas.getContext("2d");
     context.clearRect(0, 0, canvas.width, canvas.height);
 }
+
+c.addEventListener("click",randRectangles);
