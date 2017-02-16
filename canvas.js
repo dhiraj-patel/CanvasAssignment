94c0ed68dd729a292ac7a5f8976ff6581d17aedf
dhiraj-patel <dhirajsuperhero@gmail.com>
2017-02-16 10:50:08 -0500

    fixed a big in the conncet the dots code

--- canvas.js
+++ canvas.js
@@ -1,18 +1,24 @@
 var c = document.getElementById("brownies");
+var stop = document.getElementById("canvasButt");
 var ctx = c.getContext("2d");
-
-
-function randRectangles(e){
-    var randWidth = 22+Math.random()*100;
-    var randHeight = 22+Math.random()*100;
-    ctx.fillStyle = "#EEE8AA";
-    ctx.fillRect(e.offsetX-25, e.offsetY - 25, 50, 50);
-}
 
 function clearDaCanvas(){
     var canvas=document.getElementById("brownies");
     var context=canvas.getContext("2d");
     context.clearRect(0, 0, canvas.width, canvas.height);
+    context.beginPath();
 }
+var liner = function(e){
+    ctx.lineTo(e.offsetX,e.offsetY);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(e.offsetX,e.offsetY, 10, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fillStyle = "#41f4ca";
+    ctx.fill();
+    ctx.beginPath();
+    ctx.moveTo(e.offsetX,e.offsetY);
+} ;
 
-c.addEventListener("click",randRectangles);
+c.addEventListener("click",liner);
+stop.addEventListener("click",clearDaCanvas);
